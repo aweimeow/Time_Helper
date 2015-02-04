a9e52c520ac69d9bdc86939bf752a3b4e74b60f4
aweimeow <ccps960718456@gmail.com>
2015-02-04 11:23:11 +0800

    init

---
+++ TimeHelper/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace TimeHelper
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 設計工具所需的變數。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清除任何使用中的資源。
+		/// </summary>
+		/// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 設計工具產生的程式碼
+
+		/// <summary>
+		/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+		/// 修改這個方法的內容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.btn_start = new System.Windows.Forms.Button();
+			this.btn_End = new System.Windows.Forms.Button();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.Timer_GetData = new System.Windows.Forms.Timer(this.components);
+			this.SuspendLayout();
+			// 
+			// btn_start
+			// 
+			this.btn_start.Font = new System.Drawing.Font("Comic Sans MS", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btn_start.Location = new System.Drawing.Point(12, 12);
+			this.btn_start.Name = "btn_start";
+			this.btn_start.Size = new System.Drawing.Size(262, 131);
+			this.btn_start.TabIndex = 0;
+			this.btn_start.Text = "Timer Start";
+			this.btn_start.UseVisualStyleBackColor = true;
+			this.btn_start.Click += new System.EventHandler(this.button_start_Click);
+			// 
+			// btn_End
+			// 
+			this.btn_End.Font = new System.Drawing.Font("Comic Sans MS", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btn_End.Location = new System.Drawing.Point(12, 149);
+			this.btn_End.Name = "btn_End";
+			this.btn_End.Size = new System.Drawing.Size(262, 131);
+			this.btn_End.TabIndex = 1;
+			this.btn_End.Text = "Timer Pause";
+			this.btn_End.UseVisualStyleBackColor = true;
+			this.btn_End.Click += new System.EventHandler(this.btn_End_Click);
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 18;
+			this.listBox1.Location = new System.Drawing.Point(280, 12);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(360, 274);
+			this.listBox1.TabIndex = 2;
+			// 
+			// Timer_GetData
+			// 
+			this.Timer_GetData.Interval = 1000;
+			this.Timer_GetData.Tick += new System.EventHandler(this.Timer_GetData_Tick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(652, 301);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.btn_End);
+			this.Controls.Add(this.btn_start);
+			this.Name = "Form1";
+			this.Text = "Time Helper";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btn_start;
+		private System.Windows.Forms.Button btn_End;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.Timer Timer_GetData;
+	}
+}
+
---
+++ TimeHelper/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace TimeHelper
+{
+	public partial class Form1 : Form
+	{
+		HookFunction hook = new HookFunction();
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button_start_Click(object sender, EventArgs e)
+		{
+			Timer_GetData.Enabled = true;
+		}
+
+		private void btn_End_Click(object sender, EventArgs e)
+		{
+			Timer_GetData.Enabled = false;
+		}
+
+		private void Timer_GetData_Tick(object sender, EventArgs e)
+		{
+			listBox1.Items.Add(hook.GetWindowText());
+		}
+
+	}
+
+	public class HookFunction
+	{
+		[DllImport("user32.dll")]
+		static extern IntPtr GetForegroundWindow();
+		[DllImport("user32.dll")]
+		static extern int GetWindowText(IntPtr hWnd, StringBuilder lpText, int nCount);
+
+		//Return the top window's hWnd
+		private IntPtr GetTopWindow()
+		{
+			IntPtr hwnd = GetForegroundWindow();
+			return hwnd;
+		}
+
+		//Return the top window's title
+		public String GetWindowText()
+		{
+			IntPtr hwnd = GetTopWindow();
+			var str = new StringBuilder(256);
+			GetWindowText(hwnd, str, str.Capacity);
+			return str.ToString();
+		}	
+
+	}
+}
